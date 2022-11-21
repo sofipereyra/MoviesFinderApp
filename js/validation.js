5c3345c98a21e29af9a8bc6e890944e491305583
Sofía Pereyra <sofi.pereyra23@gmail.com>
2022-11-21 16:17:44 -0300

    update validation.js

--- js/validation.js
+++ js/validation.js
@@ -2,9 +2,27 @@
 const mail = document.getElementById("email");
 const pass = document.getElementById("password");
 
+const form = document.querySelector(".form-login");
+
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
+
+function validateEmail(mail) {
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value))
+   {
+     return (true)
+   }
+     alert("You have entered an invalid email address!")
+     return (false)
+}
+function validatePassword(validationPassword){
+    if (/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(pass.value)) {
+        return (true)
+    }
+    alert("You have entered an invalid password!")
+    return (false)
+}
 
 function login(email, password){
     var raw = JSON.stringify({
@@ -27,3 +45,13 @@ function login(email, password){
               window.location.href = './home.html'})
         .catch(error => console.log('error', error));
 }
+
+
+form.addEventListener('submit', (e) =>{
+  e.preventDefault();
+  if(validateEmail(mail)&& validatePassword(pass)){
+    login(mail, pass);
+  }else {
+    console.log("oops");
+  }
+})
